a0d16ac1c99ec10ba303a96a12f66c2a22dc49ef
Raghad <raghadmustafa96@yahoo.com>
2021-05-03 00:37:58 +0300

    add lab 26

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './footer.scss';
+import './header.scss';
+import './App.scss';
+
+import Header from './Header'
+import Footer from './Footer'
+import Main from './Main'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
